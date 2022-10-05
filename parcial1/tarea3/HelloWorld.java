c5d2f4dce9f949d7ba7d3af7df37b40777e5a2b8
Gael <gaelmm68@hotmail.com>
2022-10-04 19:16:26 -0500

    HolaMundo

---
+++ parcial1/tarea3/HelloWorld.java
@@ -0,0 +1,9 @@
+// Programa: Hola Mundo
+//Author: Hector
+//Institucion:Universidad Veracruzana
+
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hola, Mundo"); 
+    }
+}
