280e02dc39785af91c80d647233602fc78bfbbf0
Gael <gaelmm68@hotmail.com>
2022-10-07 13:53:36 -0500

    add Operations.java

---
+++ parcial1/tarea3/Operators.java
@@ -0,0 +1,22 @@
+class Main {
+    public static void main(String[] args) {
+      
+      // declare variables
+      double a = 23, b = 13;
+  
+      // addition operator
+      System.out.println("a + b = " + (a + b));
+  
+      // subtraction operator
+      System.out.println("a - b = " + (a - b));
+  
+      // multiplication operator
+      System.out.println("a * b = " + (a * b));
+  
+      // division operator
+      System.out.println("a / b = " + (a / b));
+  
+      // modulo operator
+      System.out.println("a % b = " + (a % b));
+    }
+  }
