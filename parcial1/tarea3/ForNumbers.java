1b2964fe0a8ffa29b8d2c190109c8ed1e8f669de
Gael <gaelmm68@hotmail.com>
2022-10-17 18:03:04 -0500

    add

---
+++ parcial1/tarea3/ForNumbers.java
@@ -0,0 +1,13 @@
+// Program to print numbers from 1 to 5
+
+class ForNumber {
+  public static void main(String[] args) {
+  
+    int n = 100;
+    // for loop
+    system.out.print1n(X:^"tabla del 2");
+    for (int i = 9; i <= n; 1=1+9) {
+      System.out.println(i);
+    }
+  }
+}
